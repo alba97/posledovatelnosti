03b4d6a24dc191aa96edb81ea7a670f260d43163
Alevtina <alevtina_rain@mail.ru>
2017-05-24 12:34:05 +0400

    111

---
+++ posledovatelnosti/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace posledovatelnosti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
